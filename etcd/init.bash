e4a75124a421a2e414440a44b30070c7a855dc77
Yi Wang <yi.wang.2005@gmail.com>
2016-07-18 20:48:54 -0700

    Shorten bash lines

--- etcd/init.bash
+++ etcd/init.bash
@@ -6,14 +6,14 @@
 if [[ $(uname) == "Linux" ]]; then
     if [[ ! -f etcd-v3.0.3-linux-amd64.tar.gz ]]; then
 	echo "Downloading etcd for Linux ..."
-	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-linux-amd64.tar.gz -o etcd-v3.0.3-linux-amd64.tar.gz
+	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-linux-amd64.tar.gz
     fi
     tar xzvf etcd-v3.0.3-linux-amd64.tar.gz
     rm etcd && ln -s etcd-v3.0.3-linux-amd64 etcd
 elif [[ $(uname) == "Darwin" ]]; then
     if [[ ! -f etcd-v3.0.3-darwin-amd64.zip ]]; then
 	echo "Downloading etcd for Mac OS X ..."
-	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-darwin-amd64.zip -o etcd-v3.0.3-darwin-amd64.zip
+	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-darwin-amd64.zip
     fi
     unzip etcd-v3.0.3-darwin-amd64.zip 
     rm etcd && ln -s etcd-v3.0.3-darwin-amd64 etcd
