e850af7c56a353c1374249b612d65a03fc612ab6
Yi Wang <yi.wang.2005@gmail.com>
2016-07-18 17:45:31 -0700

    Add new part -- etcd.md

---
+++ etcd/etcd.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for (( i = 1; i <= 3; i++ )); do
+    ./etcd/etcd \
+	--name infra$i \
+	--advertise-client-urls https://localhost:2379$i \
+	--listen-client-urls https://localhost:2379$i \
+	--cert-file ./server.crt \
+	--key-file ./server.key \
+	--client-cert-auth \
+	--ca-file ca.crt \
+	\
+	--initial-advertise-peer-urls https://localhost:2380$i \
+	--listen-peer-urls https://localhost:2380$i \
+	--peer-cert-file ./server.crt \
+	--peer-key-file ./server.key \
+	--peer-ca-file ./ca.crt \
+	\
+	--initial-cluster 'infra1=https://localhost:23801,infra2=https://localhost:23802,infra3=https://localhost:23803' \
+	--initial-cluster-token etcd-for-k8sp-tls-demo \
+	--initial-cluster-state new 2>&1 | tee /tmp/$i.log &
+done
+
---
+++ etcd/init.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Download and unpack etcd.
+#
+if [[ $(uname) == "Linux" ]]; then
+    if [[ ! -f etcd-v3.0.3-linux-amd64.tar.gz ]]; then
+	echo "Downloading etcd for Linux ..."
+	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-linux-amd64.tar.gz -o etcd-v3.0.3-linux-amd64.tar.gz
+    fi
+    tar xzvf etcd-v3.0.3-linux-amd64.tar.gz
+    rm etcd && ln -s etcd-v3.0.3-linux-amd64 etcd
+elif [[ $(uname) == "Darwin" ]]; then
+    if [[ ! -f etcd-v3.0.3-darwin-amd64.zip ]]; then
+	echo "Downloading etcd for Mac OS X ..."
+	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-darwin-amd64.zip -o etcd-v3.0.3-darwin-amd64.zip
+    fi
+    unzip etcd-v3.0.3-darwin-amd64.zip 
+    rm etcd && ln -s etcd-v3.0.3-darwin-amd64 etcd
+else
+    echo "Unknow OS: " $(uanme)
+    exit 1
+fi
+
+# 
+# Create TLS assets
+# 
+../bidirectional/create_tls_asserts.bash
